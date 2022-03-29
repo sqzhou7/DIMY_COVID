8ee4c68954c74c2a669277e285129feec0f63056
Shaoqian Zhou <z5244813@student.unsw.edu.au>
2022-03-29 15:32:45 +1100

    switch to Python

--- Attacker.java
+++
@@ -1,5 +0,0 @@
-public class Attacker {
-    public static void main(String[] args) {
-        System.out.println("system hello");
-    }
-}
---
+++ src/DimyServer.py
@@ -0,0 +1,12 @@
+from socket import *
+from threading import Thread
+import sys, select
+
+
+
+
+
+
+
+if __name__ == "main":
+    print("yes")
