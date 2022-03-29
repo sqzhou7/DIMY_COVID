9e7d149864590269c86ea8dfce5ea8e610f35de5
Shaoqian Zhou <z5244813@student.unsw.edu.au>
2022-03-29 16:14:56 +1100

    starter codes

--- src/DimyServer.py
+++ src/DimyServer.py
@@ -3,6 +3,17 @@ from threading import Thread
 import sys, select
 
 
+# acquire server host and port from command line parameter
+if len(sys.argv) != 3:
+    print("\n===== Usage: python3 DimyServer.py SERVER_ADDRESS SERVER_PORT ======\n")
+    exit(0)
+serverHost = sys.argv[1]
+serverPort = int(sys.argv[2])
+serverAddress = (serverHost, serverPort)
+
+# define socket for the server side and bind address
+serverSocket = socket(AF_INET, SOCK_STREAM)
+serverSocket.bind(serverAddress)
 
 
 
