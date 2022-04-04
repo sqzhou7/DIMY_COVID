8e6fbe846f474e4a9e61f2a65468f965f68f7585
Shaoqian Zhou <z5244813@student.unsw.edu.au>
2022-04-04 10:39:04 +1000

    basic socket codes

--- src/DimyServer.py
+++ src/DimyServer.py
@@ -1,6 +1,10 @@
+from multiprocessing.connection import wait
 from socket import *
 from threading import Thread
 import sys, select
+import time
+
+
 
 
 # acquire server host and port from command line parameter
@@ -17,7 +21,64 @@ serverSocket.bind(serverAddress)
 
 
 
+class ClientThread(Thread):
+    def __init__(self, clientAddress, clientSocket):
+        Thread.__init__(self)
+        self.clientAddress = clientAddress
+        self.clientSocket = clientSocket
+        self.clientAlive = False
+        
+        print("===== New connection created for: ", clientAddress)
+        self.clientAlive = True
+        
+    def run(self):
+        message = ''
+        
+        while self.clientAlive:
+            # use recv() to receive message from the client
+            data = self.clientSocket.recv(1024)
+            message = data.decode()
+            
+            # if the message from client is empty, the client would be off-line then set the client as offline (alive=Flase)
+            if message == '':
+                self.clientAlive = False
+                print("===== the user disconnected - ", clientAddress)
+                break
+            
+            # handle message from the client
+            if message == 'login':
+                print("[recv] New login request")
+                self.process_login()
+            elif message == 'download':
+                print("[recv] Download request")
+                message = 'download filename'
+                print("[send] " + message)
+                self.clientSocket.send(message.encode())
+            else:
+                print("[recv] " + message)
+                print("[send] Cannot understand this message")
+                message = 'Cannot understand this message'
+                self.clientSocket.send(message.encode())
+    
+    """
+        You can create more customized APIs here, e.g., logic for processing user authentication
+        Each api can be used to handle one specific function, for example:
+        def process_login(self):
+            message = 'user credentials request'
+            self.clientSocket.send(message.encode())
+    """
+    def process_login(self):
+        message = 'user credentials request'
+        print('[send] ' + message)
+        self.clientSocket.send(message.encode())
 
 
-if __name__ == "main":
-    print("yes")
+print("\n===== Server is running =====")
+print("===== Waiting for connection request from clients...=====")
+
+
+while True:
+    serverSocket.listen()
+    clientSockt, clientAddress = serverSocket.accept()
+    clientThread = ClientThread(clientAddress, clientSockt)
+    clientThread.start()
