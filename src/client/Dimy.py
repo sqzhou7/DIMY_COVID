f21e4911dff21b98cc3d7cf8e22821d029746a3c
Shaoqian Zhou <z5244813@student.unsw.edu.au>
2022-04-07 01:17:12 +1000

    message dropping and identity checking

--- src/client/Dimy.py
+++ src/client/Dimy.py
@@ -10,9 +10,10 @@ from threading import Thread
 import time
 import sys
 import os
+import random
 
-from numpy import broadcast
 
+BROADCAST_PORT = 56000
 
 def EphID_gen():
     return get_random_bytes(16)
@@ -43,14 +44,17 @@ class Message_Listener(Thread):
         self.UDP_client.bind(("", self.port))
         self.alive = True
         print("===== Message Listener up and running on")
-        print(self.UDP_client.getsockname())
+       # print(self.UDP_client.getsockname())
         
 
     def run(self):
         while (self.alive):
             data, addr = self.UDP_client.recvfrom(1024)
             print("received message: %s" %hexlify(data))
-            print(addr)
+            sender_identity_bytes = data[len(data) - 5:len(data)]
+            
+            sender_identity_str = sender_identity_bytes.decode()
+            print(sender_identity_str)
             
 """
     Seperate thread to broadcast EphID shares
@@ -61,7 +65,8 @@ class EphID_Broadcast(Thread):
         # create a UDP server socket for broadcasting
         self.UDP_server = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)
         self.UDP_server.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
-        self.broadcast_port = self.UDP_server.getsockname[1] 
+        #self.broadcast_port = self.UDP_server.getsockname()[1]
+        #print(self.UDP_server.getsockname())
         self.UDP_server.settimeout(0.2)
         #self.UDP_server.bind(("127.0.0.255", self.port))
         self.alive = True
@@ -74,10 +79,15 @@ class EphID_Broadcast(Thread):
             print("EphID generated: %s" % self.EphID)
             shares = Shamir.split(3, 5, self.EphID)
             for idx, share in shares:
-                # broadcast a chunk every 3 seconds
-                # 
-                print("Index #%d: %s broadcasted" % (idx, hexlify(share)))
-                self.UDP_server.sendto(share, ('<broadcast>', self.port))
+                # broadcast a share every 3 seconds
+                # the share has a 50% chance to get dropped
+                rand_val = random.randrange(0,2)
+                print(rand_val)
+                if rand_val == 1: 
+                    print("Index #%d: %s broadcasted" % (idx, hexlify(share)))
+                    self.UDP_server.sendto(share + identity_bytes, ('<broadcast>', BROADCAST_PORT))
+                else:
+                    print("EphID share dropped")
                 time.sleep(3)
     
     def get_broadcast_port(self):
@@ -86,12 +96,12 @@ class EphID_Broadcast(Thread):
 
 
 # Server would be running on the same host as Client
-if len(sys.argv) != 3:
+if len(sys.argv) != 1:
     print("\n===== Error usage, python3 Dimy.py SERVER_IP SERVER_PORT ======\n")
     exit(0)
-serverHost = sys.argv[1]
-serverPort = int(sys.argv[2])
-serverAddress = (serverHost, serverPort)
+#serverHost = sys.argv[1]
+#serverPort = int(sys.argv[2])
+serverAddress = ("127.0.0.1", 55000)
 
 
 """
@@ -104,7 +114,14 @@ client_TCP_socket = socket(AF_INET, SOCK_STREAM)
 # build connection with the server
 client_TCP_socket.connect(serverAddress)
 
+identity_port = client_TCP_socket.getsockname()[1]
+identity_str = str(identity_port)
+identity_bytes = identity_str.encode()
+print(identity_bytes)
+
+print(identity_port)
 print("------------connected to the server---------------")
+
 # Generate a 16-Byte Ephemeral ID
 # create an ECDH instance
 
@@ -120,4 +137,5 @@ client_listener = Message_Listener()
 client_broadcaster.start()
 client_listener.start()
 
-broadcast_port = client_broadcaster.get_broadcast_port()
+#broadcast_port = client_broadcaster.get_broadcast_port()
+#print(broadcast_port)
